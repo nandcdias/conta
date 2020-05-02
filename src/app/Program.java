e0eebf46078eb4c1ba6a204280807836b9906aae
nandcdias <fernando_c_dias@outlook.com>
2020-05-02 20:54:43 -0300

    Primeiro commit

--- src/app/Program.java
+++ src/app/Program.java
@@ -11,7 +11,7 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         Account account;
-
+        System.out.println("testandooooo");
         System.out.println("Enter account number:");
         int number = sc.nextInt();
         System.out.println("Enter account holder:");
